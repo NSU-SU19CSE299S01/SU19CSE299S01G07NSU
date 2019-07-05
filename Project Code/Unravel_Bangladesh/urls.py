662b21ee7b0c1456c3aaf4f1f1b408cd20371968
Riftabin Kabir <riftabin.kabir@northsouth.edu>
2019-07-05 21:37:15 +0600

    Solving issue #25 : Update django-setting
    Updated the  default settings for the backend connection including views, urls, settings etc.

--- Project Code/Unravel_Bangladesh/urls.py
+++ Project Code/Unravel_Bangladesh/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',include('homepage.urls')),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^menu/',include('menu.urls')),
+
 ]
---
+++ Project Code/menu/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from menu import views
+
+app_name= 'menu'
+
+urlpatterns=[
+    url(r'^$',views.menu,name='menu'),
+
+]
+
