e2d6a503569edfa3df2142f629f78e548086dd1d
Riftabin Kabir <riftabin.kabir@northsouth.edu>
2019-08-15 21:08:30 +0600

    Feature #49  : Update community for new post
    Updated the community app's views and urls and made a new definition for posting new blog and post in the community. Also added the url config.

--- Project Code/Unravel_Bangladesh/settings.py
+++ Project Code/Unravel_Bangladesh/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'menu',
     'tour',
     'community',
+    'crispy_forms',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- Project Code/community/views.py
+++ Project Code/community/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import render, redirect
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -8,3 +10,15 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'community/post_detail.html'
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+
+    else:
+        form = PostForm()
+    return render(request, 'community/post_edit.html', {'form': form})
