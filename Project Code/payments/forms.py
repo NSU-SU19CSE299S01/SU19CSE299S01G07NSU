5a2d4b44a04046c5c840cddb3d218ccd2df6a7cc
Shorno9519 <51054864+Shorno9519@users.noreply.github.com>
2019-08-17 01:03:23 +0600

    Feature #21 : Update forms.py for payments app
    Update forms.py till Addressform part of the page

--- Project Code/payments/forms.py
+++ Project Code/payments/forms.py
@@ -1,1 +1,26 @@
+from django import forms
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit, Row, Column, Field
+
+
+STATES = (
+    ('', 'Choose...'),
+    ('DHK', 'Dhaka'),
+    ('SYL', 'Sylhet'),
+    ('RAJ', 'Rajshahi'),
+    ('BAR', 'Barisal'),
+    ('KHU', 'Khulna'),
+    ('CTG', 'Chittagong'),
+)
+
+class AddressForm(forms.Form):
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput())
+    address_1 = forms.CharField(label='Address', widget=forms.TextInput(attrs={'placeholder': '1234 Main St'}))
+    address_2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Apartment, studio, or floor'}))
+    Amount = forms.CharField()
+    state = forms.ChoiceField(choices=STATES)
+    TrxID = forms.CharField(label='BKash-01920101801',widget=forms.TextInput(attrs={'placeholder': 'TrxID'}))
+    check_me_out = forms.BooleanField(required=False)
 
