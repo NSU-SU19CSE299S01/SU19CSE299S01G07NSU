1ab3e6c9952694ff8c12b25fe6752db2e93b9ad1
Riftabin Kabir <riftabin.kabir@northsouth.edu>
2019-08-17 01:11:32 +0600

    Feature #21  : Update views.py file of payments app
    Updated the views.py file till Crispy part of  payments form in the payments app

--- Project Code/payments/views.py
+++ Project Code/payments/views.py
@@ -1,3 +1,25 @@
+from django.views.generic import FormView, TemplateView
+from django.urls import reverse_lazy
+from django.conf import settings
+from .forms import AddressForm, CrispyAddressForm, CustomFieldForm
+import stripe
 from django.shortcuts import render
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class AddressFormView(FormView):
+    form_class = AddressForm
+    success_url = reverse_lazy('success')
+
+
+    def get_context_data(self, **kwargs):  # new
+        context = super().get_context_data(**kwargs)
+        context['key'] = settings.STRIPE_PUBLISHABLE_KEY
+        return context
+
+
+class CrispyAddressFormView(FormView):
+    form_class = CrispyAddressForm
+    success_url = reverse_lazy('success')
+    template_name = 'payments/crispy_form.html'
+
